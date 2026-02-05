6b71f8b57a9f64ee36ee075a8099c8f3b3a2c059
sujalkumar27 <sujal31122005@gmail.com>
2026-02-05 12:21:11 +0530

    testing configurations

--- src/main/java/com/sujal/item_manager/ItemManagerApplication.java
+++ src/main/java/com/sujal/item_manager/ItemManagerApplication.java
@@ -8,6 +8,7 @@ public class ItemManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemManagerApplication.class, args);
+		System.out.println("MY Project is Started!");
 	}
 
 }
